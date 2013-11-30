1529366ece616c0f68c97f2ad0c3a50368ecd84f
Yurijmi <me@yurijmi.ru>
2013-11-30 18:51:06 +0400

    Finished SignIn

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,9 +30,14 @@ describe "Authentication" do
       end
 
       it { should have_selector('title', text: user.name) }
-      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Your profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
 
     describe "after visiting another page" do

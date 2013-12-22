3d3e5998268547cf980004f133b817b30eee6310
yurijmi <me@yurijmi.ru>
2013-12-22 15:45:01 +0400

    Index in Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
-  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :signed_in_user, only: [:edit,  :update]
+  before_filter :correct_user,   only: [:edit,  :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+
+  def index
+
+  end
 
   def show
     @user = User.find(params[:id])

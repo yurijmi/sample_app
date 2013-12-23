370621d6bcba55ff201a8001db9c34884f87a311
yurijmi <me@yurijmi.ru>
2013-12-24 01:59:12 +0400

    Refactored Home Page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,10 @@ class StaticPagesController < ApplicationController
     if signed_in?
       @micropost = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
+
+      render 'home_user'
+    else
+      render 'home_guest'
     end
   end
 

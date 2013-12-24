b94da1ecbd083d08a7bb5b9d9376e25aa231b69c
yurijmi <me@yurijmi.ru>
2013-12-24 17:54:08 +0400

    Specs for following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -74,6 +74,16 @@ describe "Authentication" do
             before { delete micropost_path(FactoryGirl.create(:micropost)) }
             specify { response.should redirect_to(signin_path) }
           end
+
+          describe "visiting the following page" do
+            before { visit following_user_path(user) }
+            it { should have_selector('title', text: 'Sign in') }
+          end
+
+          describe "visiting the followers page" do
+            before { visit followers_user_path(user) }
+            it { should have_selector('title', text: 'Sign in') }
+          end
         end
 
         describe "after signing in" do

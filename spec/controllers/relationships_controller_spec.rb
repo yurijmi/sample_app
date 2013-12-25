21736c57c68e6cd27f9d56993f8127d8e39d3f24
yurijmi <me@yurijmi.ru>
2013-12-25 21:04:06 +0400

    Another deprecation

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -12,7 +12,7 @@ describe RelationshipsController do
     it "should increment the Relationship count" do
       expect do
         xhr :post, :create, relationship: { followed_id: other_user.id }
-      end.should change(Relationship, :count).by(1)
+      end.to change(Relationship, :count).by(1)
     end
 
     it "should respond with success" do

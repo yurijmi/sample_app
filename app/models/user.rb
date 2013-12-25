005c5af2cbe37e8ebcc68329d10270194e1282e8
yurijmi <me@yurijmi.ru>
2013-12-25 21:03:37 +0400

    Not really fast and secure realisation for micropost feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,8 +34,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def feed
-    # Это предварительное решение. См. полную реализацию в "Following users".
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
